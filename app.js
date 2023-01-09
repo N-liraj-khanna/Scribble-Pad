39a63a8c93efbca8d5f805ac81fc1f6abf66cdd6
N Liraj Khanna <lirajkhanna2002@gmail.com>
2023-01-09 17:37:48 +0530

    Server Side Socket Connection initiated

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-const express = require('express');
-const app=express();
+// Constants
+const PORT = process.env.PORT || 5000;
+
+// Imports
+const express = require("express");
+const app = express();
+const io = require("socket.io")(PORT, {
+  cors: {
+    origin: "https://localhost/3000",
+    method: ["GET", "POST"],
+  },
+});
+
+
+// Connections
+io.on("connection", (socket) => {
+  console.log("User Connected");
+  console.log(socket);
+});
 
 app.get("/", (req, res) => {
   return res.send("Welcome to your collabrative scribble pad");
-})
+});
 
-app.listen(5000, ()=>{
-  console.log("Server Up and Running on port 5000");
-})
+// app.listen(5000, () => {
+//   console.log("Server Up and Running on port 5000");
+// });
